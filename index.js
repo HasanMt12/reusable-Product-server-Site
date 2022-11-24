a4cc37dd3de393b356381819e388b5cffc111514
HasanMt12 <hasan.2mahmud22gmail.com>
2022-11-24 13:10:26 +0600

    basic mongodb set up

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -9,6 +9,23 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gniuvqv.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        // const productCollections = client.db('reusableProductSell').collection('sellProduct')
+    }
+    finally{
+
+    }
+}
+run().catch(console.log);
+
 
 app.get('/', async(req, res) =>{
     res.send('usedProduct  server is running');
