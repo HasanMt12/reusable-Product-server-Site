61f2c93de511d71458037c222846f07c41bef83b
HasanMt12 <hasan.2mahmud22gmail.com>
2022-11-24 11:51:05 +0600

    basic server site set up

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async(req, res) =>{
+    res.send('usedProduct  server is running');
+})
+
+app.listen(port, () => console.log(`used product sell site server portal running on ${port}`))
