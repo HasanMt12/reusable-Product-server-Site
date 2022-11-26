55559faa276778793bd77c8327a67b74ed4bf3af
HasanMt12 <hasan.2mahmud22gmail.com>
2022-11-26 23:57:28 +0600

    jwt token use and seller verifiaction added and admin security added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const app = express();
@@ -16,6 +17,23 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next){
+   
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorize access');
+    }
+    const token =  authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run(){
     try{
         const productCollections = client.db('reusableProductSell').collection('sellProduct')
@@ -23,14 +41,14 @@ async function run(){
         const reservationCollections = client.db('reusableProductSell').collection('reservation')
         const usersCollections = client.db('reusableProductSell').collection('users')
          
-            // get all categories
+                     // get all categories and show HOME page
           app.get("/categories", async (req, res) => {
               const query = {};
               const categories = await categoryCollections.find(query).toArray();
               res.send(categories);
           });
 
-           // get single category product by name
+                    // get single category product by name and show dynamic route
             app.get("/categories/:name", async (req, res) => {
                 const name = req.params.name;
                 const query = { categoryName: name };
@@ -38,8 +56,7 @@ async function run(){
                 res.send(result);
             });
 
-            //reservation/bookings post by mongoDb
-
+                    //reservation/bookings post by and add database
         app.post('/reservation', async (req, res) => {
             const reservation = req.body
             console.log(reservation);
@@ -47,27 +64,69 @@ async function run(){
             res.send(result);
         })
 
-        app.get('/reservation', async (req, res) => {
+                    //get reservation data and send client side
+        app.get('/reservation', verifyJWT, async (req, res) => {
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+
+            if(email !== decodedEmail){
+                return res.status(403).send({message: 'forbidden access'});
+            }
             const query = { email: email };
             const reservation = await reservationCollections.find(query).toArray();
             res.send(reservation);
         })
 
+
+        app.get('/jwt', async(req , res) => {
+            const email = req.query.email;
+            const query = {email: email};
+            const user = await usersCollections.findOne(query);
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '10d'})
+                return res.send({accessToken: token})
+            }
+            console.log(user);
+            res.status(403).send({accessToken: ''})
+        })
+
+                    // post all users and add database
         app.post('/users', async(req , res) => {
             const user = req.body;
-           
             const result = await usersCollections.insertOne(user);
             res.send(result);
         })
         
+                    //get all users by database and send data to client side
         app.get('/users' , async(req , res) =>{
             const query = {} ; 
             const users = await usersCollections.find(query).toArray();
             res.send(users);
         })
 
-        app.pa
+        app.get('/users/admin/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollections.findOne(query);
+            res.send({ isAdmin: user?.role === 'admin'});
+        })
+        
+        // update seller account to verified
+        app.put('/users/admin/:id',  async(req , res) => {
+           
+            const id = req.params.id;
+              const filter = {_id:ObjectId(id)}
+              const options = {upsert: true};
+              const updatedDoc = {
+                $set: {
+                  verification: 'verify'
+                }
+              }
+              const result = await usersCollections.updateOne(filter,  updatedDoc, options);
+              res.send(result);
+           });
+
+           
     }
     finally{
 
