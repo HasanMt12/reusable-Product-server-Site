1934c623727ee644ae5472e0da640b7d3831cb8b
Hasan Mt <hasan.2mahmud22@gmail.com>
2023-04-15 10:17:56 +0600

    added

--- index.js
+++ index.js
@@ -18,22 +18,24 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@clu
 // console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-function verifyJWT(req, res, next){
-   
-    const authHeader = req.headers.authorization;
-    if(!authHeader){
-        return res.status(401).send('unauthorize access');
+ // Jwt Token
+ function verifyJWT(req, res, next) {
+  console.log("token inside verifyJWT", req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  // bearer  eta split(" ") kora hoilo
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
     }
-    const token =  authHeader.split(' ')[1];
+    req.decoded = decoded;
+    next();
+  });
+}  
 
-    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
-        if(err){
-            return res.status(403).send({message: 'forbidden access'})
-        }
-        req.decoded = decoded;
-        next();
-    })
-}
 
 async function run(){
     try{
